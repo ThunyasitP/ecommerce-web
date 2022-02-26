3e938ce2a4dbe7fec4fe7ed460a8bfa14feada98
Thunyasit.P <thunyasit.p@gmail.com>
2022-02-26 18:16:41 +0700

    fix syntax check string null, empty and len where condition

--- src/main/java/com/company/ecommerce/product/repository/ProductRepository.java
+++ src/main/java/com/company/ecommerce/product/repository/ProductRepository.java
@@ -13,7 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(value = "SELECT p FROM Product p")
     List<Product> findAllProducts();
 
-    @Query(value = "SELECT p FROM Product p WHERE p.name LIKE %:product% OR LOWER(p.name) LIKE %:product%")
+    @Query(value = "SELECT p FROM Product p WHERE ( p.name LIKE %:product% OR LOWER(p.name) LIKE %:product% ) AND ( p.name <> '' AND  p.name IS NOT NULL AND LEN(p.name) > 0 ) " )
     List<Product> findProductByName( @Param("product")  String product);
 
 }
--- src/test/java/com/company/ecommerce/product/controller/ProductControllerTest.java
+++ src/test/java/com/company/ecommerce/product/controller/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.company.ecommerce.product.controller;
 
 import com.company.ecommerce.product.repository.Product;
 import com.company.ecommerce.product.repository.ProductRepository;
+import com.company.ecommerce.product.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -86,6 +87,142 @@ class ProductControllerTest {
         ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products", ProductResponse[].class);
         List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
 
+        // Assert
+        assertEquals(0, result.size());
+    }
+
+
+    @Test
+    @DisplayName("ส่ง London จะได้ผลลัพธ์ 1 รายการ")
+    void success_find_product_by_name_show_1_products() {
+        // Arrange
+        Product product1 = new Product(1, "Boy London T-Shirt", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        List<Product> responses = new ArrayList<>();
+        responses.add(product1);
+        when(productRepository.findProductByName("London")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/London", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง Shirt จะได้ผลลัพธ์ 2 รายการ")
+    void success_find_product_by_name_show_2_products() {
+        // Arrange
+        Product product1 = new Product(1, "Boy London T-Shirt", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        Product product4 = new Product(4, "H&M T-Shirt", "https://www.shutterstock.com/de/image-photo/new-york-circa-march-2016-close-444910246", 299, 0, 2000, "2022-02-16 08:30:00", "2022-02-16 08:30:00");
+        List<Product> responses = new ArrayList<>();
+        responses.add(product1);
+        responses.add(product4);
+        when(productRepository.findProductByName("Shirt")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/Shirt", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง TEST จะได้ผลลัพธ์ 0 รายการ")
+    void success_find_product_by_name_show_0_products() {
+        // Arrange
+        List<Product> responses = new ArrayList<>();
+        when(productRepository.findProductByName("TEST")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/TEST", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง shirt จะได้ผลลัพธ์ 2 รายการ")
+    void success_find_product_by_lowercase_show_1_products() {
+        // Arrange
+        Product product1 = new Product(1, "Boy London T-Shirt", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        Product product4 = new Product(4, "H&M T-Shirt", "https://www.shutterstock.com/de/image-photo/new-york-circa-march-2016-close-444910246", 299, 0, 2000, "2022-02-16 08:30:00", "2022-02-16 08:30:00");
+        List<Product> responses = new ArrayList<>();
+        responses.add(product1);
+        responses.add(product4);
+        when(productRepository.findProductByName("shirt")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/shirt", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง H&M T-Shirt จะได้ผลลัพธ์ 1 รายการ")
+    void success_find_product_by_fullname_show_1_products() {
+        // Arrange
+        Product product4 = new Product(4, "H&M T-Shirt", "https://www.shutterstock.com/de/image-photo/new-york-circa-march-2016-close-444910246", 299, 0, 2000, "2022-02-16 08:30:00", "2022-02-16 08:30:00");
+        List<Product> responses = new ArrayList<>();
+        responses.add(product4);
+        when(productRepository.findProductByName("H&M T-Shirt")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/H&M T-Shirt", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง เสื้อยืด จะได้ผลลัพธ์ 3 รายการ")
+    void success_find_product_by_thai_show_3_products() {
+        // Arrange
+        Product product6 = new Product(6, "เสื้อยืดตราห่าน", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        Product product7 = new Product(7, "เสื้อยืดตราปุ๋ย", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        Product product8 = new Product(8, "เสื้อยืดสีขาว", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        List<Product> responses = new ArrayList<>();
+        responses.add(product6);
+        responses.add(product7);
+        responses.add(product8);
+        when(productRepository.findProductByName("เสื้อยืด")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/เสื้อยืด", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง เสื้อแขนยาว จะได้ผลลัพธ์ 0 รายการ")
+    void success_find_product_by_thai_show_0_products() {
+        // Arrange
+        List<Product> responses = new ArrayList<>();
+        when(productRepository.findProductByName("เสื้อแขนยาว")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/เสื้อแขนยาว", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง '' จะได้ผลลัพธ์ 0 รายการ")
+    void success_find_product_by_null_show_0_products() {
+        // Arrange
+        List<Product> responses = new ArrayList<>();
+        when(productRepository.findProductByName("")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
+        // Assert
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("ส่ง '' จะได้ผลลัพธ์ 0 รายการ เมื่อไม่มีข้อมูลใด ๆ")
+    void success_find_product_by_null_and_db_null_show_0_products() {
+        // Arrange
+        List<Product> responses = new ArrayList<>();
+        when(productRepository.findProductByName("")).thenReturn(responses);
+        // Act
+        ResponseEntity<ProductResponse[]> responseEntity = testRestTemplate.getForEntity("/products/", ProductResponse[].class);
+        List<ProductResponse> result = Arrays.asList(responseEntity.getBody());
         // Assert
         assertEquals(0, result.size());
     }
