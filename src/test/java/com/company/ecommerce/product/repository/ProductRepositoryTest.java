5b3b5b32ac809fccb7027e01482ea369a95af521
Thunyasit.P <thunyasit.p@gmail.com>
2022-02-26 17:02:52 +0700

    test: product repository test find all product

---
+++ src/test/java/com/company/ecommerce/product/repository/ProductRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.company.ecommerce.product.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName("findAllProducts()")
+    void findAllProducts(){
+        // Arrange
+        Product product1 = new Product(6, "Boy London T-Shirt", "https://www.shutterstock.com/de/image-vector/grunge-london-flag-tshirt-printing-vector-337267853", 3500, 500, 50, "2022-01-13 08:30:00", "2022-01-13 08:30:00");
+        productRepository.save(product1);
+        // Act
+        List<Product> result = productRepository.findAllProducts();
+
+        // Assert
+        assertEquals(6, result.size());
+    }
+
+
+}
