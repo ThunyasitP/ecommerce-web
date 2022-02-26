e99dc552fca5bbe3b280c05021a4d599edccfc69
Thunyasit.P <thunyasit.p@gmail.com>
2022-02-26 17:16:05 +0700

    add find product by name in repository

--- src/main/java/com/company/ecommerce/product/repository/ProductRepository.java
+++ src/main/java/com/company/ecommerce/product/repository/ProductRepository.java
@@ -12,4 +12,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query(value = "SELECT p FROM Product p")
     List<Product> findAllProducts();
 
+    @Query(value = "SELECT p FROM Product p WHERE p.name = ?1")
+    List<Product> findProductByName(String name);
+
 }
